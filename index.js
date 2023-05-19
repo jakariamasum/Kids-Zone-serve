c3c3add99d150028904cf928f883e170e4fa7f78
jakariamasum <jmphy698@gmail.com>
2023-05-20 00:51:59 +0600

    all toys data finds

--- index.js
+++ index.js
@@ -30,6 +30,15 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const toyCollection= client.db('toyCollection').collection('toys');
+
+        app.get('/toys',async(req,res)=>{
+            const result=await toyCollection.find().toArray(); 
+            // console.log(result)
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
