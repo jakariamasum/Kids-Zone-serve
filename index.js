90ff34e5342b2a8f397731aeb3f7d6ec2ca4229c
jakariamasum <jmphy698@gmail.com>
2023-05-20 11:08:27 +0600

    user specific data loaded

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -45,9 +45,22 @@ async function run() {
             const result = await myToysCollection.insertOne(newToy)
         })
         app.get('/my-toys', async (req, res) => {
-            const result = await toyCollection.find().toArray();
+            // const query=(req.query.email)
+            const data = req.query;
+            let query = {};
+            if (req.query?.email) {
+                query = { sellerEmail: req.query.email }
+            }
+            const result = await myToysCollection.find(query).toArray();
             // console.log(result)
             res.send(result)
+        })
+        app.get('/toy-details/:id',async(req,res)=>{
+            const id=req.params.id; 
+            const query={_id: new ObjectId(id)}; 
+            const result=await toyCollection.findOne(query);
+            console.log(result)
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
